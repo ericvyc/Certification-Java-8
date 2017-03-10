6c5508b99f4c16340d75c4350b98ae9ac06539cb
ericvyc <eric_vilar2007@hotmail.com>
2017-03-10 17:26:22 -0500

    Destroying objects.

---
+++ src/com/certification/chap1/DestroyingObjects.java
@@ -0,0 +1,17 @@
+package com.certification.chap1;
+public class DestroyingObjects {
+	public static void main(String[] args) {
+		String one, two;
+		one = new String("a");
+		two = new String("b");
+		one = two;
+		String three = one;
+		one = null;
+	}
+	
+	@Override
+	protected void finalize() throws Throwable {
+		// TODO Auto-generated method stub
+		super.finalize();
+	}
+}
